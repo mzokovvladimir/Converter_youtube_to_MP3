0fb8ed12fb4e3a8e01f64f35bf9d67033fe5155f
Vladimir Mzokov <mzokovvladimir@gmail.com>
2022-07-31 17:26:13 +0300

    Converter Youtube to MP3 version 1.0

---
+++ convert_youtube_to_mp3.py
@@ -0,0 +1,17 @@
+# python convert_youtube_to_mp3.py <URL>
+import youtube_dl
+import sys
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+if __name__ == "__main__":
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        filenames = sys.argv[1:]
+        ydl.download(filenames)
